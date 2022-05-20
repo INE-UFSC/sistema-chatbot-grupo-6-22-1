3c82265f0caf6959fa099d420b7d21d412ae0918
joaopedrotheodoro <joaopedrth@gmail.com>
2022-05-19 21:31:43 -0300

    Bots
    Mais Bots adicionados

--- app.py
+++ app.py
@@ -2,9 +2,12 @@
 from SistemaChatBot import SistemaChatBot as scb
 from Bots.BotEspanhol import BotEspanhol
 from Bots.BotIngles import BotIngles
+from Bots.BotCachorro import BotCachorro
+from Bots.BotBola import BotBola
+from Bots.BotBinario import BotBinario
 
 ###construa a lista de bots disponíveis aqui
-lista_bots = [BotEspanhol("Juan"), BotIngles("John")]
+lista_bots = [BotEspanhol("Juan"), BotIngles("John"), BotCachorro("Theo"), BotBola("Bola"), BotBinario("01000101 01100100 01110101")]
 
 sys = scb.SistemaChatBot("CrazyBots",lista_bots)
 sys.inicio()
