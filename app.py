9179fb34f8795ea617c25318d82efbefe7cd6edc
Matheus S <matheussouza4555@gmail.com>
2022-05-20 10:05:58 -0300

    Integrado BotEspelhado(não lembro de qual grupo)

--- app.py
+++ app.py
@@ -5,13 +5,14 @@ from Bots.BotIngles import BotIngles
 from Bots.BotCachorro import BotCachorro
 from Bots.BotBola import BotBola
 from Bots.BotBinario import BotBinario
+from Bots.BotEspelhado import BotEspelhado
 
 
 ###construa a lista de bots disponíveis aqui
 lista_bots = [BotEspanhol("Juan"), BotIngles("John"), BotCachorro("Theo"), BotBola("Bola"), BotBinario("01000101 01100100 01110101")]
 
 #Bots integrados
-#lista_bots.append(BotZangado("Zangado"))
+lista_bots.append(BotEspelhado("Espelhado"))
 
 sys = scb.SistemaChatBot("CrazyBots",lista_bots)
 sys.inicio()
